62bdecd477c4e4f0c4e324366fe0288340bf612c
ColtGriff2k <coltgriff2k@yahoo.com>
2025-10-14 20:54:51 -0600

    Added and Updated HTML and CSS files
    Added and Updated HTML and CSS files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, World! I am an alert box!");
+<script>
+	function My_First_Function() {
+		var String= "Drive Me!"
+		var result= String.fontcolor("blue");
+		document.getElementById("Car").innerHTML=
+		reuslt;
+	}
+</script>
