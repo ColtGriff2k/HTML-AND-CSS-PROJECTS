6884fbce513df8dcb7a5816d66a897dfe57f8214
ColtGriff2k <coltgriff2k@yahoo.com>
2026-01-19 09:10:53 -0700

    Updated JS

--- Academy-Cinemas/javascript/javascript.js
+++ Academy-Cinemas/javascript/javascript.js
@@ -68,7 +68,7 @@ if ($(document).scrollTop()>50){
 $(document).ready(function(){
     //On click when an element contains just the nav-link class and not the dropdown toggle and then
     //also close when when an element with the class .dropdown-item (each movie link) has been clicked
-    $("navbar-nav").on('click', 'nav-link:not(".dropdown-toggle"), .dropdown-item', function(){
+    $(".navbar-nav").on('click', '.nav-link:not(".dropdown-toggle"), .dropdown-item', function(){
         //Collapse the navbar when a link or dropdown item is clicked
         $(".navbar-collapse").collapse('hide');
     });
